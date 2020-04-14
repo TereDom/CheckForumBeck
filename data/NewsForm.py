c4d6815a177cb6ea782ccad9d5e009e507e4a362
Димка <mityka2004@yandex.ru>
2020-04-14 21:22:51 +0300

    Созданы форма форума и форма добавления записи

---
+++ data/NewsForm.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class NewsForm(FlaskForm):
+    title = StringField('Заголовок', validators=[DataRequired()])
+    content = TextAreaField("Содержание")
+    submit = SubmitField('Применить')
