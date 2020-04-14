1befd026d71c868fc43bb291d3c24d72d2feb056
Антон <antonovka2310@yandex.ru>
2020-04-14 19:39:39 +0300

    Добавленны базовые элементы

--- app.py
+++ app.py
@@ -1,17 +1,57 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
+from flask_login import login_user
+from werkzeug.utils import redirect
+from data.LoginForm import LoginForm
+from data.RegisterForm import RegisterForm
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
-    app.run(host='127.0.0.1', port=8080)
+    app.run(port=8080, host='127.0.0.1')
 
 
-@app.route('/')
-def index():
-    return "CheckForumBeck"
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        # session = db_session.create_session()
+        # if session.query(User).filter(User.email == form.email.data).first():
+        #     return render_template('register.html', title='Регистрация',
+        #                            form=form,
+        #                            message="Такой пользователь уже есть")
+        # user = User(
+        #     name=form.name.data,
+        #     email=form.email.data,
+        #     about=form.about.data
+        # )
+        # user.set_password(form.password.data)
+        # session.add(user)
+        # session.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    # if form.validate_on_submit():
+    #     session = db_session.create_session()
+    #     user = session.query(User).filter(User.email == form.email.data).first()
+    #     if user and user.check_password(form.password.data):
+    #         login_user(user, remember=form.remember_me.data)
+    #         return redirect("/")
+    #     return render_template('login.html',
+    #                            message="Неправильный логин или пароль",
+    #                            form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
