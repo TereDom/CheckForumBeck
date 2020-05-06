e065c8e12a7820bf4abd0164fc1f3092bfdf8428
Димка <mityka2004@yandex.ru>
2020-05-06 13:20:33 +0300

    Добавлены мета-теги

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def main():
     """Запуск приложения"""
     logging.info('Приложение запущено')
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
 
 
 @login_manager.user_loader
